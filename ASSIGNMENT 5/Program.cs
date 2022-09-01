a96acf629b0df36d1fabf5341b3a086fe86a1fa2
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:38:44 +0530

    Add files via upload

--- ASSIGNMENT 5/Program.cs
+++ ASSIGNMENT 5/Program.cs
@@ -1,1 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the bit number ");//10010<--18
+            string num = Console.ReadLine();
+            int ans = 0;
+           
+            ans = Convert.ToInt32(num, 2);
+            Console.WriteLine(ans);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* */
+//  char c = num[i];
+// ans+ = Convert.ToInt32(num[i]) * Pow(2, x);
