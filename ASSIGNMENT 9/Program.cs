93a82a17df41b082c05cd2dc97075924375b8363
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:41:06 +0530

    Add files via upload

--- ASSIGNMENT 9/Program.cs
+++ ASSIGNMENT 9/Program.cs
@@ -1,1 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter one Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool flg=true;
+            for(int i = 2; i<=num/2; i++)
+            {
+                if(num%i == 0)
+                {
+                    Console.WriteLine(" not a prime number");
+                    flg = false;
+                    break;
+                }   
+            }
+            if (flg)
+            {
+                Console.WriteLine("prime number");                
+            }
+        }
+    }
+}
