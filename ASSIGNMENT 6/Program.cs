70ed2e56f35cfda020db1b0be9f12543ea90f7cc
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:39:17 +0530

    Add files via upload

--- ASSIGNMENT 6/Program.cs
+++ ASSIGNMENT 6/Program.cs
@@ -1,1 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string of two letters");
+            string twoLetters=Console.ReadLine();
+            string output = "";
+            for (char i = twoLetters[0]; i <= twoLetters[1]; i++)
+            {
+                output += i;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
