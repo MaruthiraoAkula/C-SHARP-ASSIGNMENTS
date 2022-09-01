82fbfd71d264ec16d81b442fff239d8fac262a6a
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:36:29 +0530

    Add files via upload

--- ASSIGNMENT 3/Program.cs
+++ ASSIGNMENT 3/Program.cs
@@ -1,1 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment3
+{
+    public class Sorted
+    {
+        public  bool Method(params int[] Arrr)
+        {
+            int j;
+            bool flag1 = false;
+            bool flag2 = false;
+            for (j = 1; j < Arrr.Length; j++)
+            {
+                if (Arrr[j-1] < Arrr[j])
+                {
+                    flag1 = true;
+                }
+                if (Arrr[j - 1] > Arrr[j])
+                {
+                    flag2 = true;
+                }
+
+            }
+            if (flag1 && flag2)
+            {
+                return false;
+            }
+            /*  if (flag1 && !flag2)
+              {
+                  return true;
+              }
+              if (!flag1 && flag2)
+              {
+                  return true;
+              }*/
+            return true;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter Three Intergers for Array");
+            int[] Arr = new int[3];
+            for (int i=0;i<Arr.Length;i++)
+            {
+                int Number = Convert.ToInt32(Console.ReadLine());
+                Arr[i]=Number;
+            }
+            Sorted obj=new Sorted();
+            bool returnn=obj.Method(Arr);
+            Console.WriteLine(returnn);
+
+        }
+    }
+}
