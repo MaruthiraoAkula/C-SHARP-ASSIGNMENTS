7f8cc4292f85f582e55b0a287dfe5ab403630fed
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:39:53 +0530

    Add files via upload

--- ASSIGNMENT 7/Program.cs
+++ ASSIGNMENT 7/Program.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment7
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter size of integers");
+
+            int intsize=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter "+intsize+" numbers");
+            List<int> list = new List<int>(intsize);
+            for (int i = 0; i < intsize; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                list.Add(num);
+            }
+            List<int> distinct = list.Distinct().ToList();
+            var Queryresult = ((from x in distinct orderby x descending select x).Take(5));
+            Console.WriteLine("Top 5 ekements :");
+            foreach (var item in Queryresult)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
