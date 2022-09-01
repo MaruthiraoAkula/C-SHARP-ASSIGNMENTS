ab1a39b257ee2f5f4414f97b407e570271f96e55
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 17:17:45 +0530

    Update Program.cs

--- ASSIGNMENT 4/Program.cs
+++ ASSIGNMENT 4/Program.cs
@@ -10,10 +10,12 @@ namespace Assignment4
     {
         public bool IsNumberContain(int num,int digit)
         {
+            int inputNum = num;
             while (num > 0)
             {
                 if (num % 10 == digit) // 1234 % 10 = 4 ,123 % 10=3,
                 {
+                    Console.WriteLine("Input Number is {0} ",num);
                     return true;
                 }
                 num /= 10; // 1234/10=123
@@ -27,8 +29,9 @@ namespace Assignment4
         {
             int digit = 3;
             Console.WriteLine("Enter numbers");
+            
             int num = Convert.ToInt32(Console.ReadLine());
-            Numbercontain obj=new Numbercontain();
+            Numbercontain obj=new Numbercontain();           
             bool res=  obj.IsNumberContain(num, digit);
             Console.WriteLine(res);
         }
