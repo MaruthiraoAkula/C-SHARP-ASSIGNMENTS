e1bf1a3a81c6a1b78ae38f1ee6f78c07e5efe39e
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:38:05 +0530

    Add files via upload

--- ASSIGNMENT 4/Program.cs
+++ ASSIGNMENT 4/Program.cs
@@ -1,1 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Assignment4
+{
+    class Numbercontain
+    {
+        public bool IsNumberContain(int num,int digit)
+        {
+            while (num > 0)
+            {
+                if (num % 10 == digit) // 1234 % 10 = 4 ,123 % 10=3,
+                {
+                    return true;
+                }
+                num /= 10; // 1234/10=123
+            }return false;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = 3;
+            Console.WriteLine("Enter numbers");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Numbercontain obj=new Numbercontain();
+            bool res=  obj.IsNumberContain(num, digit);
+            Console.WriteLine(res);
+        }
+    }
+}
