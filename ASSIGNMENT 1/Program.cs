19740211b415e510e579dede4d7378c0dc2c2e6b
MaruthiraoAkula <108869247+MaruthiraoAkula@users.noreply.github.com>
2022-09-01 11:21:26 +0530

    Update Program.cs

--- ASSIGNMENT 1/Program.cs
+++ ASSIGNMENT 1/Program.cs
@@ -1,1 +1,30 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            int number;
+            Console.WriteLine("Enter your Number");
+
+            number = Convert.ToInt32(Console.ReadLine());
+            result = number%2;
+            if (result == 0)
+            {
+                Console.WriteLine("Even ");
+            }
+            else
+            Console.WriteLine("odd ");
+      
+
+        }
+    }
+}
